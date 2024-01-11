0b7f278367dba75e3e6f0052ce333092af6552dc
zdebyman <yuriy.zdebskyi@gmail.com>
2024-01-11 16:30:47 -0500

    debug log

--- client/src/features/gratitudes/NewGratitudeForm.js
+++ client/src/features/gratitudes/NewGratitudeForm.js
@@ -2,12 +2,13 @@ import { useNavigate } from "react-router-dom";
 import { createGratitude } from "../../services/gratitudeService";
 import GratitudeForm from "./GratitudeForm";
 import { objectToFormData } from "../../utils/formDataHelper";
-
+import { API_URL } from "../../constants";
 function NewGratitudeForm() {
   const navigate = useNavigate();
 
   const handleCreateSubmit = async (rawData) => {
     try {
+      console.log("handleCreateSubmit + " + API_URL )
       const formData = objectToFormData({ gratitude: rawData });
       const response = await createGratitude(formData);
       navigate(`/gratitudes/${response.id}`);
